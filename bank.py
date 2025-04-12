bddd9d90cd18bb0316d1fc42988276908009f2c1
DemigodKS <mankok222@gmail.com>
2025-04-12 12:06:05 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,6 @@
+import account
 from exceptions import AccountNotFoundError, AccountExist
-from account import BankAccount
+from account import BankAccount, SavingsAccount
 from loguru import logger
 
 
@@ -10,6 +11,7 @@ class Bank:
 
     def add_account(self, account: BankAccount) -> None:
         try:
+
             if account._account_number not in self.accounts:
               self.accounts[account._account_number] = account._balance
               logger.info(f'Добавлен новый счет № {account._account_number}: {account._balance} руб.')
@@ -20,29 +22,46 @@ class Bank:
 
     def accounts_all(self):
         logger.info(self.accounts)
+        return self.accounts
 
-    def transfer(self, from_acc: str, to_acc: str, amount: float) -> None:
+    def transfer(self, from_acc: BankAccount, to_acc: BankAccount, amount: float) -> None:
 
         try:
-            if from_acc in self.accounts and to_acc in self.accounts:
-                logger.info(f'Переведено {amount} руб. со счета № {from_acc} на счет № {to_acc}.')
+            #баланс заложен в self.accounts
+            if from_acc._account_number in self.accounts and to_acc._account_number in self.accounts:
+                logger.info(f'Перевод {amount} руб. со счета № {from_acc._account_number} на счет № {to_acc._account_number}.')
+
+                if from_acc._balance > amount:
+                    from_acc.withdraw(amount)
+                    to_acc.deposit(amount)
+                    self.accounts[from_acc._account_number] = from_acc._balance
+                    self.accounts[to_acc._account_number] = to_acc._balance
+                else:
+                    from_acc.withdraw(amount)
+
             else:
                 raise AccountNotFoundError
         except AccountNotFoundError:
-            if from_acc not in self.accounts and to_acc not in self.accounts:
-                logger.error(f'Данные счета {from_acc}, {to_acc} не существуют!')
-            elif from_acc not in self.accounts:
-                logger.error(f'Счет № {from_acc} не существует!')
+            if from_acc._account_number not in self.accounts and to_acc._account_number not in self.accounts:
+                logger.error(f'Данные счета {from_acc._account_number}, {to_acc._account_number} не существуют!')
+            elif from_acc._account_number not in self.accounts:
+                logger.error(f'Счет № {from_acc._account_number} не существует!')
             else:
-                logger.error(f'Счет № {to_acc} не существует!')
+                logger.error(f'Счет № {to_acc._account_number} не существует!')
 
-#ff = Bank()
-#dd = BankAccount('cчёт № SFD-001', 1000)
-#dd1 = BankAccount('cчёт № SFD-003', 1000)
-#ff.add_account(dd)
-#ff.add_account(dd1)
-#ff.accounts_all()
-#ff.transfer('cчёт № SFD-003', 'cчёт № SFD-002', 500)
+
+#
+# ff = Bank()
+# dd = BankAccount('SFD-001', 1500)
+# dd1 = SavingsAccount('SFD-003', 1000)
+# ff.add_account(dd)
+# ff.add_account(dd1)
+# ff.accounts_all()
+# ff.transfer(from_acc=dd, to_acc=dd1, amount=200)
+# dd.get_balance()
+# dd1.get_balance()
+# ff.accounts_all()
+
 
 
 
